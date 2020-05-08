974c12de57b9cc35d05076d38608a21de0a08b02
ErikaTog <erika.togniolo@gmail.com>
2020-05-08 16:21:57 -0300

    subindo model usuario teste

---
+++ band_express/models/Usuario.js
@@ -0,0 +1,56 @@
+const Usuario = (sequelize, DataTypes) => {
+    let usuario = sequelize.define(
+        'Usuario', 
+        {
+            id_usuario: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            nome: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            senha: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            data_cadastro: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            admin: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false
+            },
+            avatar: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            wallpaper: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            link_perfil: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            perfil_id_perfil: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        }, {
+            tableName: "usuario",
+            timestamps: false
+        })
+
+    return usuario;
+};
+
+module.exports = Usuario;
