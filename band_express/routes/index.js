7bafb0979a221e2661e02c12c1eb6efd3abc6273
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-08 22:49:49 -0300

    Feito as alterações de html >> ejs. Criado o partial - header e footer

--- band_express/routes/index.js
+++ band_express/routes/index.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/perfil/1', (req, res, next) => res.render('perfil-musico'));
+
 module.exports = router;
