ead765a65bc7f1df8b5e92f9d4ab43ead9528c9d
andressa fazam <andressafazam@gmail.com>
2020-05-09 00:41:06 -0300

    perfil-estab.html >> ejs

--- band_express/routes/index.js
+++ band_express/routes/index.js
@@ -2,22 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) { res.render('index', { title: 'Express' }); });
 
-router.get('/perfil/1', (req, res, next) => res.render('perfil-musico'));
+router.get('/', (req, res, next) => res.render('index'));
 
-router.get('/perfil-editar/1', (req, res, next) => res.render('perfil-musico-editar'));
+router.get('/cadastro/2', (req, res, next) => res.render('form-banda'));
 
 router.get('/feed', (req, res, next) => res.render('feed'));
 
 router.get('/bate-papo', (req, res, next) => res.render('bate-papo'));
 
-router.get('/fale-conosco', (req, res, next) => res.render('fale-conosco'));
-
-router.get('/cadastro/2', (req, res, next) => res.render('form-banda'));
-
 router.get('/anuncie', (req, res, next) => res.render('anuncie'));
+
+router.get('/perfil/1', (req, res, next) => res.render('perfil-musico'));
+
+router.get('/perfil-editar/1', (req, res, next) => res.render('perfil-musico-editar'));
+
+router.get('/perfil/3', (req, res, next) => res.render('perfil-estab'));
+
+router.get('/fale-conosco', (req, res, next) => res.render('fale-conosco'));
 
 module.exports = router;
