5d0f216e94cdcf717fc477b272a700aca803a992
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-11 12:02:47 -0300

    Correção das relações

--- band_express/models/Tecnico.js
+++ band_express/models/Tecnico.js
@@ -18,11 +18,13 @@ const Tecnico = (sequelize, DataTypes) => {
         }
     );
 
-    tecnico.belongsToMany(models.Musico, { 
-        through: 'musico_tecnicos', 
-        foreignKey: 'id_tecnico',
-        as: 'musicos'
-    });
+    instrumento.associate = models => {
+        tecnico.belongsToMany(models.Musico, { 
+            through: 'musico_tecnicos', 
+            foreignKey: 'id_tecnico',
+            as: 'musicos'
+        });
+    }
 
     return tecnico;
 };
