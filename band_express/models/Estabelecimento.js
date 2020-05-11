4d32b7f23895fa7cd4f3f6447aafa0243e000379
andressa fazam <andressafazam@gmail.com>
2020-05-11 00:31:29 -0300

    Add colunas no model Estabelecimento

--- band_express/models/Estabelecimento.js
+++ band_express/models/Estabelecimento.js
@@ -1,7 +1,50 @@
 const Estabelecimento = (sequelize, DataTypes) => {
     let estabelecimento = sequelize.define(
         'Estabelecimento', 
-        {}, {
+        {
+            id_estab: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            categoria: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            sobre: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            estado: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            cidade: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            site: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            telefone: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            funcionamento: {
+                type: DataTypes.BOOLEAN,
+                allowNull: true
+            },
+            id_usuario: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        }, {
             tableName: "estabelecimento",
             timestamps: false
         })
