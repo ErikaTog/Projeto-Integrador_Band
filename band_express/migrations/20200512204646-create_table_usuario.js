231f318f32ed07451f7f949cbc59cd8ac8d1c7ea
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-12 18:03:03 -0300

    Criado migração usuario

---
+++ band_express/migrations/20200512204646-create_table_usuario.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'usuario', 
+      { 
+        id_usuario: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING(200),
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: Sequelize.STRING(200),
+            allowNull: false,
+            unique: true
+        },
+        senha: {
+            type: Sequelize.STRING(256),
+            allowNull: false
+        },
+        data_cadastro: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        id_estado: {
+          type: Sequelize.INTEGER,
+          allowNull: false, 
+          references: {
+            model: 'estado',
+            key: 'id_estado'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        id_cidade: {
+          type: Sequelize.INTEGER,
+          allowNull: false, 
+          references: {
+            model: 'cidade',
+            key: 'id_cidade'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+        avatar: {
+            type: Sequelize.STRING(256),
+            allowNull: true
+        },
+        wallpaper: {
+            type: Sequelize.STRING(256),
+            allowNull: true
+        },
+        link_perfil: {
+            type: Sequelize.STRING(100),
+            allowNull: true
+        },
+        id_tipos_perfil: {
+          type: Sequelize.INTEGER,
+          allowNull: false, 
+          references: {
+            model: 'tipos_perfil',
+            key: 'id_tipos_perfil'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        } 
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('usuario');
+  }
+};
