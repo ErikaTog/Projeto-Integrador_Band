f4d1aecc13432bf12854b34e5d97e61dbf56adbe
andressa fazam <andressafazam@gmail.com>
2020-05-13 02:16:37 -0300

    Add rotas para o cadastro do Estabelecimento

--- band_express/routes/cadastro.js
+++ band_express/routes/cadastro.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const cadastroController = require('../controllers/CadastroController');
 const cadastroMusicoController = require('../controllers/CadastroMusicoController');
+const cadastroEstabController = require('../controllers/CadastroEstabController');
 
 
 /* GET pre-cadastro. */
@@ -15,5 +16,9 @@ router.post('/banda', cadastroController.saveBanda)
 /* cadastro-músico */
 router.get('/musico', cadastroMusicoController.formMusician);
 router.post('/musico', cadastroMusicoController.saveMusician);
+
+/* cadastro-estab */
+router.get('/estabelecimento', cadastroEstabController.formEstab);
+router.post('/estabelecimento', cadastroEstabController.saveEstab);
 
 module.exports = router;
