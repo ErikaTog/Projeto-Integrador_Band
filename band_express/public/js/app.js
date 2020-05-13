aa9e1e764f6237129ae66c09a6f7b70257ea128f
ErikaTog <erika.togniolo@gmail.com>
2020-05-13 13:31:22 -0300

    incluindo função para testar o submit do formBanda

--- band_express/public/js/app.js
+++ band_express/public/js/app.js
@@ -7,3 +7,12 @@ submitForms = function() {
     document.getElementById("form6").submit();
     document.getElementById("form7").submit();
 };
+
+submitFormsBanda = function() {
+    document.getElementById("form1").submit();
+    document.getElementById("form2").submit();
+    document.getElementById("form3").submit();
+    document.getElementById("form4").submit();
+    document.getElementById("form5").submit();   
+
+};
