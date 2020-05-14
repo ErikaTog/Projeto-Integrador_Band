160745319f043e496fd16324e3b515886944db38
ErikaTog <erika.togniolo@gmail.com>
2020-05-14 02:05:40 -0300

    criando tabela banda

---
+++ band_express/migrations/20200514042240-create_table_banda.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('banda', {
+      id_banda: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      genero: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      sobre: {
+        type: Sequelize.STRING(2200),
+        allowNull: true
+      },
+      site: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      canal: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'usuario',
+          key: 'id_usuario'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('banda');
+  }
+};
