3caccf6129dca5c7402087cf589a9bfc310fedba
andressa fazam <andressafazam@gmail.com>
2020-05-15 02:34:06 -0300

    Criado o model minha_rede

---
+++ band_express/models/Minha_rede.js
@@ -0,0 +1,28 @@
+const Minha_rede = (sequelize, DataTypes) => {
+    let minha_rede = sequelize.define(
+        'Minha_rede', 
+        {
+            id_minha_rede: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            id_usuario: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            id_usuario_seguido: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        }, {
+            tableName: "minha_rede",
+            timestamps: false
+        }
+    );
+    
+    return minha_rede;
+};
+
+module.exports = Minha_rede;
