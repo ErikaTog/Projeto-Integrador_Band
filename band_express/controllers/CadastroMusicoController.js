1b0cf0bf8ce4c1266e2316f6ff253ddca8bcbc33
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-16 08:56:09 -0300

    Validação do campo nome

--- band_express/controllers/CadastroMusicoController.js
+++ band_express/controllers/CadastroMusicoController.js
@@ -1,21 +1,45 @@
-const { Cidade, Usuario, Musico } = require('../models');
+const { Estado, Cidade, Usuario, Musico } = require('../models');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 const cadastroMuicoController = {
     formMusician: (req, res) => {
         return res.render('form-musico');
     },
+    validations: (req, res) => {
+        // check('nome')
+        //     .isEmpty().withMessage('Esse campo não pode ser vazio')
+        //     .isLength({ min: 2, max:100 }).withMessage('Esse campo deve ter entre 2 a 100 caracteres')
+        //     .custom(async (value, { req }) => {
+        //         let user = await Usuario.findOne({ nome: value });
+            
+        //         if (!_.isEmpty(user)) {
+        //           return false;
+        //         }
+        //       })
+        //       .withMessage("Este usuário já existe")
+        
+        let errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.render('form-musico', { errors: errors.errors });
+        }
+    },
     saveMusician: async (req, res) => {
+
         let { nome, senha, email, sexo, sobre, estado, cidade, site, canal, canto, toco, tecnico, instrumento, habilidadeTecnica } = req.body;
         // console.log(user, password, email, gender, bio, estado, cidade, site, canal, canto, toco, tecnico, instrument, techinicalSkill)
         
         senha = bcrypt.hashSync(senha, 10);
         
-        // Buscando o id da Cidade e do Estado na tabela cidade
+        // Buscando o id da Cidade na tabela cidade
         const findIdCidade = await Cidade.findOne({ where: { nome: cidade } })
         
         const idCidade = findIdCidade.dataValues.id_cidade;
-        const idEstado = findIdCidade.dataValues.id_estado;
+
+        // Buscando o id do Estado na tabela estado
+        const findIdEstado = await Estado.findOne({ where: { nome: estado } })
+
+        const idEstado = findIdEstado.dataValues.id_estado;
 
         // Inserindo informação na tabela usuario   
         const dadosUsuario = await Usuario.create({
--- band_express/routes/cadastro.js
+++ band_express/routes/cadastro.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 const cadastroController = require('../controllers/CadastroBandaController');
 const cadastroMusicoController = require('../controllers/CadastroMusicoController');
@@ -15,7 +16,11 @@ router.post('/banda', cadastroController.saveBanda)
 
 /* cadastro-músico */
 router.get('/musico', cadastroMusicoController.formMusician);
-router.post('/musico', cadastroMusicoController.saveMusician);
+router.post('/musico', [
+    check('nome')
+        .isEmpty().withMessage('Esse campo não pode ser vazio')
+        .isLength({ min: 2, max:100 }).withMessage('Esse campo deve ter entre 2 a 100 caracteres')
+], cadastroMusicoController.validations, cadastroMusicoController.saveMusician);
 
 /* cadastro-estab */
 router.get('/estabelecimento', cadastroEstabController.formEstab);
