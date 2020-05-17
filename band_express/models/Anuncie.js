5192386f69a0203b5a4546a3c307d6aa5be096f6
andressa fazam <andressafazam@gmail.com>
2020-05-16 22:13:31 -0300

    Add colunas na tabela

--- band_express/models/Anuncie.js
+++ band_express/models/Anuncie.js
@@ -2,7 +2,37 @@ const Anuncie = (sequelize, DataTypes) => {
     let anuncie = sequelize.define(
         'Anuncie', 
         {
-            
+            id_vagas: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            titulo: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            descricao: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            local: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            valor: {
+                type: DataTypes.FLOAT,
+                defaultValue: 0,
+                allowNull: false
+            },
+            img_anuncio: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            id_usuario: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
         }, {
             tableName: "anuncie",
             timestamps: false
