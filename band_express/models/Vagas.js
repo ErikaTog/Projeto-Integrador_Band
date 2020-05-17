2faccba01f35f2e9739555c4df37ec237f8ccf83
andressa fazam <andressafazam@gmail.com>
2020-05-16 22:13:20 -0300

    Add colunas na tabela

--- band_express/models/Vagas.js
+++ band_express/models/Vagas.js
@@ -2,7 +2,32 @@ const Vagas = (sequelize, DataTypes) => {
     let vagas = sequelize.define(
         'Vagas', 
         {
-
+            id_vagas: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            titulo: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            descricao: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            local: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            tipo_vaga: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            id_usuario: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
         }, {
             tableName: "vagas",
             timestamps: false
