a367de051e65a993709275fe61a5e57119721c01
andressa fazam <andressafazam@gmail.com>
2020-05-16 22:13:45 -0300

    Add colunas na tabela

--- band_express/models/Bate_papo.js
+++ band_express/models/Bate_papo.js
@@ -2,7 +2,28 @@ const Bate_papo = (sequelize, DataTypes) => {
     let bate_papo = sequelize.define(
         'Bate_papo', 
         {
-
+            id_bate_papo: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            mensagem: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            hora_data: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            id_usuario_remetente: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            id_usuario_destinatario: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
         }, {
             tableName: "bate_papo",
             timestamps: false
