1b0d9a7cfec50eba3823060c257439b9151f9f51
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-18 11:36:44 -0300

    Inserido os dados nas tabelas intermediárias

--- band_express/routes/cadastroMusico.js
+++ band_express/routes/cadastroMusico.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const {check, validationResult, body} = require('express-validator');
 
 const { Usuario } = require('../models');
@@ -9,19 +9,20 @@ const cadastroMusicoController = require('../controllers/CadastroMusicoControlle
 /* cadastro-músico */
 router.get('/', cadastroMusicoController.formMusician);
 router.post('/',
-[
-    check('nome').trim()
-        .not().isEmpty().withMessage('Esse campo não pode ser vazio')
-        .isLength({ min: 2, max:100 }).withMessage('Esse campo deve ter entre 2 a 100 caracteres'),
-    body('nome').trim()
-        .custom(async value => {
-            let userCheck = await Usuario.findOne( { where: {nome: value} } );
-            if (userCheck) {
-                console.log('User Exists');
-                return Promise.reject('Este usuário está em uso');
-            }
-        })
-], cadastroMusicoController.error, 
+// [
+//     check('nome').trim()
+//         .not().isEmpty().withMessage('Esse campo não pode ser vazio')
+//         .isLength({ min: 2, max:100 }).withMessage('Esse campo deve ter entre 2 a 100 caracteres'),
+//     body('nome').trim()
+//         .custom(async value => {
+//             let userCheck = await Usuario.findOne( { where: {nome: value} } );
+//             if (userCheck) {
+//                 console.log('User Exists');
+//                 return Promise.reject('Este usuário está em uso');
+//             }
+//         })
+// ], 
+// cadastroMusicoController.error, 
 cadastroMusicoController.saveMusician);
 
 
