ee41b35ba8d9d2ca8a5f4c92d22c769226848d31
andressa fazam <andressafazam@gmail.com>
2020-05-19 14:14:19 -0300

    Add migration anuncie

---
+++ band_express/migrations/20200516043911-create_table_anuncie.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'anuncie', 
+      {
+        id_anuncie: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        titulo: {
+          type: Sequelize.STRING(50),
+          allowNull: false
+        },
+        descricao: {
+          type: Sequelize.STRING(200),
+          allowNull: false
+        },
+        local: {
+          type: Sequelize.STRING(50),
+          allowNull: false
+        },
+        valor: {
+          type: Sequelize.FLOAT,
+          defaultValue: 0,
+          allowNull: false
+        },
+        img_anuncio: {
+          type: Sequelize.STRING(256),
+          allowNull: false
+        },
+        id_usuario: {
+          type: Sequelize.INTEGER,
+          allowNull: false, 
+          references: {
+            model: 'usuario',
+            key: 'id_usuario'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('anuncie');
+  }
+};
