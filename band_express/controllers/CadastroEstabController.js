2ec5d54261bde0074f6451ccdf0979c132d3e3fc
andressa fazam <andressafazam@gmail.com>
2020-05-19 20:05:20 -0300

    Tabela funcionamento agora recebe dados do form

--- band_express/controllers/CadastroEstabController.js
+++ band_express/controllers/CadastroEstabController.js
@@ -1,90 +1,123 @@
 const Sequelize = require('sequelize');
-const config = require ('../config/database');
-const { Usuario, Cidade, Estado, Estabelecimento, Funcionamento } = require('../models');
-const { check, validationResult, body } = require('express-validator');
+const config = require('../config/database');
+const {
+	Usuario,
+	Cidade,
+	Estado,
+	Estabelecimento,
+	Funcionamento
+} = require('../models');
+const {
+	check,
+	validationResult,
+	body
+} = require('express-validator');
 const bcrypt = require('bcrypt');
 
 const cadastroEstabController = {
-    formEstab: async (req, res) => {
-        //fazendo a busca de todos os estados
-        const buscaEstados = await Estado.findAll({})
+	formEstab: async (req, res) => {
+		//fazendo a busca de todos os estados
+		const buscaEstados = await Estado.findAll({})
 
-        //criando uma variável que vai conter a lista de estados
-        listaEstados = []
-    
-        //buscando os estados e incluindo na lista
-        buscaEstados.forEach((estado) => {
-            listaEstados.push(estado.dataValues.uf)
-        })
+		//criando uma variável que vai conter a lista de estados
+		listaEstados = []
 
-        return res.render('form-estab', {estados: listaEstados});
-    },
+		//buscando os estados e incluindo na lista
+		buscaEstados.forEach((estado) => {
+			listaEstados.push(estado.dataValues.uf)
+		})
 
-    saveEstab: async(req, res) => {
-        
-        let { nome, senha, email, servico, sobre, estado, cidade, site, 
-            emailEstab, telefone, inputFuncionamento, inputAbertura, inputFechamento} = req.body;
+		return res.render('form-estab', {
+			estados: listaEstados
+		});
+	},
 
-        nome = nome.trim(); 
-        senha = senha.trim();
-        email = email.trim();
-        servico = servico.trim();
-        sobre = sobre.trim();
-        estado = estado.trim();
-        cidade = cidade.trim(); 
-        site = site.trim();
-        emailEstab = emailEstab.trim(); 
-        telefone = telefone.trim();
-        // inputFuncionamento = inputFuncionamento.trim();
-        inputAbertura = inputAbertura.trim();
-        inputFechamento = inputFechamento.trim();
+	saveEstab: async (req, res) => {
 
-        // Buscando o id_cidade e id_estado na tabela cidade
-        const findIdCidade = await Cidade.findAll({
-            //Inner join
-            include: [{
-                // com a tabela Estado
-                model: Estado, 
-                // utilizando a chave estabelecida na associação do model Cidade com Estado, cujo alias é 'estado'
-                as: 'estado',
-                // com um filtro adicional do estado que o usuário digitou
-                where: {
-                    uf: estado
-                }
-                }],
-                // e um último filtro da cidade que o usuário digitou
-            where: { 
-                nome: cidade 
-            },
-        });
+		let {
+			nome,
+			senha,
+			email,
+			servico,
+			sobre,
+			estado,
+			cidade,
+			site,
+			emailEstab,
+			telefone,
+			inputFuncionamento,
+			inputAbertura,
+			inputFechamento
+		} = req.body;
 
-        const idCidade = findIdCidade[0].dataValues.id_cidade;
-        const idEstado = findIdCidade[0].dataValues.id_estado;
+		nome = nome.trim();
+		senha = senha.trim();
+		email = email.trim();
+		servico = servico.trim();
+		sobre = sobre.trim();
+		estado = estado.trim();
+		cidade = cidade.trim();
+		site = site.trim();
+		emailEstab = emailEstab.trim();
+		telefone = telefone.trim();
+		inputFuncionamento = inputFuncionamento.trim();
+		inputAbertura = inputAbertura.trim();
+		inputFechamento = inputFechamento.trim();
 
-        // Inserindo informação na tabela usuario   
-        const dadosUsuario = await Usuario.create({
-            nome,
-            email,
-            senha: bcrypt.hashSync(senha, 10),
-            data_cadastro: new Date(),
-            id_cidade: idCidade,
-            id_estado: idEstado,
-            id_tipos_perfil: 3
-        })
+		// Buscando o id_cidade e id_estado na tabela cidade
+		const findIdCidade = await Cidade.findAll({
+			//Inner join
+			include: [{
+				// com a tabela Estado
+				model: Estado,
+				// utilizando a chave estabelecida na associação do model Cidade com Estado, cujo alias é 'estado'
+				as: 'estado',
+				// com um filtro adicional do estado que o usuário digitou
+				where: {
+					uf: estado
+				}
+			}],
+			// e um último filtro da cidade que o usuário digitou
+			where: {
+				nome: cidade
+			},
+		});
 
-        // Inserindo dados complementares na tabela estab
-        const dadosEstab = await Estabelecimento.create({
-            categoria: servico, 
-            sobre, 
-            site, 
-            email: emailEstab,
-            telefone, 
-            funcionamento: 0,
-            id_usuario: dadosUsuario.id_usuario
-        });
-        
-        return res.redirect('/feed')
-    }
+		const idCidade = findIdCidade[0].dataValues.id_cidade;
+		const idEstado = findIdCidade[0].dataValues.id_estado;
+
+		// Inserindo informação na tabela usuario   
+		const dadosUsuario = await Usuario.create({
+			nome,
+			email,
+			senha: bcrypt.hashSync(senha, 10),
+			data_cadastro: new Date(),
+			id_cidade: idCidade,
+			id_estado: idEstado,
+			id_tipos_perfil: 3
+		})
+
+		// Inserindo dados complementares na tabela estab
+		const dadosEstab = await Estabelecimento.create({
+			categoria: servico,
+			sobre,
+			site,
+			email: emailEstab,
+			telefone,
+			funcionamento: 0,
+			id_usuario: dadosUsuario.id_usuario
+		});
+
+		// Inserindo dados complementares na tabela músico
+		const dadosFuncionamento = await Funcionamento.create({
+			dia: inputFuncionamento,
+			horario_abertura: inputAbertura,
+			horario_fechamento: inputFechamento,
+			id_estab: dadosEstab.id_estab
+		});
+
+		return res.redirect('/feed')
+	}
 }
 
 module.exports = cadastroEstabController;
