5e3b8af0f7373707831ccfba9435bcf3b37bf89b
ErikaTog <erika.togniolo@gmail.com>
2020-05-21 23:27:02 -0300

    incluindo arquivo de rota de Minha Rede

---
+++ band_express/routes/minhaRede.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const minhaRedeController = require('../controllers/MinhaRedeController');
+const VerificaUsuarioLogado = require('../middlewares/verificaUsuarioLogado');                                            
+
+/* GET pre-cadastro. */
+router.get('/', VerificaUsuarioLogado, minhaRedeController.view); 
+
+
+module.exports = router;
