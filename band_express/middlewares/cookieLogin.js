e2394d88f12c2cb0f038737147c8301e7929a9fb
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-22 15:49:56 -0300

    Add ids na session

--- band_express/middlewares/cookieLogin.js
+++ band_express/middlewares/cookieLogin.js
@@ -7,7 +7,7 @@ const cookieLogin = async (req, res, next) => {
 
         // Procurar se há um usuário com esse e-mail
         const usuario = await Usuario.findOne({
-            attributes: ['nome', 'email', 'senha'],
+            attributes: ['id_usuario', 'nome', 'email', 'senha', 'id_tipos_perfil'],
             raw: true,
             where: {
                 email
