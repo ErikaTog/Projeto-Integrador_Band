e2e05531410c4157cf26bae990caa1892cd44954
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-24 18:36:30 -0300

    Informações na view perfil editar musico

--- band_express/controllers/PerfilEditarMusicoController.js
+++ band_express/controllers/PerfilEditarMusicoController.js
@@ -1,7 +1,112 @@
+const { Cidade, Estado, Usuario, Musico, MusicoInstrumentos, Instrumento, MusicoTecnicos, Tecnico, Minha_rede, Audio, Video } = require('../models');
 
 const perfilEditarMusicoController = {
-    show: (req, res) => {
-        res.render('perfil-musico-editar', { title: 'Perfil-editar', usuario: req.session.usuario });
+    show: async (req, res) => {
+        // Buscar informação da tabela usuario
+        const dadosUsuario = await Usuario.findOne({ 
+            where: { nome: req.session.usuario.nome },
+            raw: true,
+            attributes: ['avatar', 'wallpaper', 'cidade.nome', 'cidade.estado.uf'],
+            include: [{
+                model: Cidade,
+                as: 'cidade',
+                attributes: [],
+                include: [{
+                    model: Estado,
+                    as: 'estado',
+                    attributes: [],
+                }]
+            }]
+        });
+
+        // Buscar informação da tabela músico
+        const dadosMusico = await Musico.findOne({ 
+            where: { id_usuario: req.session.usuario.id_usuario },
+            raw: true,
+            attributes: ['id_musico', 'sobre', 'site', 'canal', 'canto', 'toco', 'tecnico'] 
+        });
+
+        // Buscar informação das habilidades - instrumentos
+        let instrumentos = [];
+
+        if (dadosMusico.toco) {
+            instrumentos = await Musico.findAll({
+                where: { id_musico: dadosMusico.id_musico },
+                raw: true,
+                attributes: ['musicos.instrumentos.instrumento'], 
+                include: [
+                    {
+                        model: MusicoInstrumentos,
+                        as: 'musicos',
+                        attributes: [],
+                        include: [
+                            {
+                                model: Instrumento,
+                                as: 'instrumentos',
+                                attributes: []
+                            }
+                        ]
+                    }
+                ],
+            });
+        };
+        
+        // Buscar informação das habilidades - tecnicos
+        let tecnicos = [];
+
+        if (dadosMusico.tecnico) {
+            tecnicos = await Musico.findAll({
+                where: { id_musico: dadosMusico.id_musico },
+                raw: true,
+                attributes: ['musicosTec.habilidade_tecnicas.habilidade_tecnica'], 
+                include: [
+                    {
+                        model: MusicoTecnicos,
+                        as: 'musicosTec',
+                        attributes: [],
+                        include: [
+                            {
+                                model: Tecnico,
+                                as: 'habilidade_tecnicas',
+                                attributes: []
+                            }
+                        ]
+                    }
+                ],
+            });
+        }
+        
+        // Buscar quantidade de seguidores e seguindo
+        const seguidores = await Minha_rede.count({ where: { id_usuario: req.session.usuario.id_usuario } });
+        const seguindo = await Minha_rede.count({ where: { id_usuario_seguido: req.session.usuario.id_usuario } });
+
+        // Buscar áudios
+        const audios = await Audio.findAll({
+            where: { id_usuario: req.session.usuario.id_usuario },
+            raw: true,
+            attributes: ['tipo', 'titulo', 'caminho'],
+            limit: 4
+        });
+
+        // Buscar vídeos
+        const videos = await Video.findAll({
+            where: { id_usuario: req.session.usuario.id_usuario },
+            raw: true,
+            attributes: ['tipo', 'titulo', 'caminho'],
+            limit: 4
+        });
+
+        res.render('perfil-musico-editar', { 
+            usuario: req.session.usuario,
+            dadosUsuario,
+            dadosMusico, 
+            instrumentos, 
+            tecnicos, 
+            seguidores, 
+            seguindo, 
+            audios, 
+            videos
+        });
     }
 }
 
