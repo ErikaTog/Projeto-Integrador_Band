f9bc0dc78db3bef5308cb2e72135557dc6f1940a
ErikaTog <erika.togniolo@gmail.com>
2020-05-24 02:46:37 -0300

    incluindo middleare para verificação de erro do modal de alterar senha

---
+++ band_express/middlewares/perfilBanda.js
@@ -0,0 +1,115 @@
+const { validationResult } = require('express-validator');
+const {Usuario, Banda, BandaIntegrantes, Cidade, Estado, Minha_rede, Audio, Video} = require('../models')
+
+const perfilBanda = {
+    error: async (req, res, next) => {
+        let errors = validationResult(req).array({ onlyFirstError: true });
+        console.log(errors)
+
+        //Pegando o id_usuario
+        let id_usuario = req.session.usuario.id_usuario
+       
+        //Pegando o nome da banda
+        let nomeBanda = req.session.usuario.nome
+     
+        //Pegando o email da banda
+        let emailBanda = req.session.usuario.email
+       
+        //Selecionando os dados da banda
+        const dadosBanda = await Banda.findOne({
+            raw: true,
+            attributes: ['id_banda', 'genero', 'sobre', 'site', 'canal'],
+            where: {
+                id_usuario
+            }
+        })
+        
+        // Selecionando o Estado, a Cidade, o Avatar e o Wallpaper da Banda na tabela usuario
+        const dadosUsuarioBanda = await Usuario.findOne({
+            raw: true,
+            attributes: ['avatar', 'wallpaper', 'cidade.nome', 'cidade.estado.uf'],
+            include: [{
+                attributes: [],
+                model: Cidade,
+                as: 'cidade',
+                include: [{
+                    attributes: [],
+                    model: Estado,
+                    as: 'estado'
+                }]
+            }],
+            where: {
+                id_usuario
+            }
+        })
+    
+        // Selecionando o nome, função e avatar dos integrantes
+        const integrantes = await BandaIntegrantes.findAll({
+            raw: true,
+            attributes: ['integrantesUsuario.nome', 'funcao', 'integrantesUsuario.avatar'],
+            include: [{
+                attributes: [],
+                model: Usuario,
+                as: 'integrantesUsuario'
+            }],
+            where: {
+                id_banda: dadosBanda.id_banda
+            }
+
+        })
+  
+        //Verificando a quantidade de usuários que a banda está seguindo
+        let seguindo = await Minha_rede.count({
+            where: {
+                id_usuario
+            }
+        });
+    
+        //Verificando a quantidade de seguidores da banda
+        let seguidores = await Minha_rede.count({
+            where: {
+                'id_usuario_seguido': id_usuario
+            }
+        });
+ 
+        //Pegando os vídeos postados
+        const videos = await Video.findAll({
+            raw: true,
+            attributes: ['tipo', 'titulo', 'caminho'],
+            where: {
+                id_usuario
+            }
+        })
+ 
+
+        //Pegando os áudios postados
+        const audios = await Audio.findAll({
+            raw: true,
+            attributes: ['tipo', 'titulo', 'caminho'],
+            where: {
+                id_usuario
+            }
+        })
+
+        if(errors.length) {
+            return res.render('perfil-banda', {
+                title: 'Perfil',
+                usuario: req.session.usuario,
+                nomeBanda,
+                emailBanda,
+                seguindo,
+                seguidores,
+                dadosBanda,
+                dadosUsuarioBanda,
+                integrantes,
+                videos,
+                audios,
+                errors: errors 
+            });
+        } 
+
+        next();
+    }
+}
+
+module.exports = perfilBanda;
