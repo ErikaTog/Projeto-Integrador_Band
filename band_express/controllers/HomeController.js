086ac852baa7004731eade1188993cc887d8b159
ErikaTog <erika.togniolo@gmail.com>
2020-05-24 21:33:08 -0300

    incluindo dados da Banda

--- band_express/controllers/HomeController.js
+++ band_express/controllers/HomeController.js
@@ -1,4 +1,4 @@
-const { Musico } = require('../models');
+const { Musico, Banda } = require('../models');
 
 const homeController = {
     view: async (req, res) => {
@@ -8,7 +8,21 @@ const homeController = {
             attributes: ['id_musico', 'sobre', 'site', 'canal', 'canto', 'toco', 'tecnico'] 
         });
 
-        return res.render('feed', { title: 'Home', usuario: req.session.usuario, dadosMusico });
+        //Selecionando os dados da banda
+        const dadosBanda = await Banda.findOne({
+        raw: true,
+        attributes: ['id_banda', 'genero', 'sobre', 'site', 'canal'],
+        where: {
+            id_usuario: req.session.usuario.id_usuario
+        }
+    })
+
+
+
+
+
+
+        return res.render('feed', { title: 'Home', usuario: req.session.usuario, dadosMusico, dadosBanda });
     }
 } 
 
