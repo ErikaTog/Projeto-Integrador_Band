8072d0e9e343bfa3536aa4c277d012186db7930b
ErikaTog <erika.togniolo@gmail.com>
2020-05-25 23:02:24 -0300

    incluindo rota put

--- band_express/routes/perfilEditarBanda.js
+++ band_express/routes/perfilEditarBanda.js
@@ -1,8 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const { check, body } = require('express-validator');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const { Usuario } = require('../models');
 const perfilEditarBandaController = require('../controllers/PerfilEditarBandaController');
 const VerificaUsuarioLogado = require('../middlewares/verificaUsuarioLogado');
+const BandaMiddleware = require('../middlewares/PerfilEditarBanda')
 
 router.get('/:id', VerificaUsuarioLogado, perfilEditarBandaController.show);
+
+router.put('/:id',
+[
+    // Validando o campo nome
+    check('nome').trim()
+        .not().isEmpty().withMessage('Queremos ajudar a sua banda a ficar famosa. Para isso, precisamos que nos diga o nome dela!')
+        .isLength({ min: 2, max: 25 }).withMessage('O nome da sua banda não tem somente uma letra, não é mesmo? Escreva, ao menos, 2 caracteres e no máximo 25!'),
+    body('nome').trim()
+        .custom(async (value, { req }) => {
+            let userCheck = await Usuario.findOne({
+                where: {
+                    nome: value,
+                    id_usuario: {
+                        [Op.ne]: req.session.usuario.id_usuario
+                    }
+                }
+            });
+            if (userCheck) {
+                return Promise.reject('As bandas cadastradas no Band+ são únicas e a sua também será. Então, por favor, nos indique outro nome!');
+            }
+        }),
+
+    // Validando o campo email
+    check('email').trim()
+        .not().isEmpty().withMessage('Hey, queremos nos comunicar com você! Diga o seu e-mail para nós!')
+        .isEmail().withMessage('Ops, você não digitou o email corretamente!'),
+    body('email').trim()
+        .custom(async (value, { req }) => {
+            let emailCheck = await Usuario.findOne({
+                where: {
+                    email: value,
+                    id_usuario: {
+                        [Op.ne]: req.session.usuario.id_usuario
+                    }
+                } 
+            });
+            if (emailCheck) {
+                return Promise.reject('Esse e-mail já foi cadastrado. Precisamos que nos informe outro.');
+            }
+        }),
+    
+    // Validando o campo Estado
+    check('estado').trim()
+        .not().isEmpty().withMessage('Queremos que sua banda faça sucesso por onde passar, mas precisamos que nos indique um Estado.'),
+    
+    // Validando o campo Cidade
+    check('cidade').trim()
+        .not().isEmpty().withMessage('Queremos que sua banda faça sucesso por onde passar, mas precisamos que nos indique uma Cidade.'),
+
+    // Validando o campo sobre
+    check('sobre').trim()
+        .isLength({ max: 2200 }).withMessage('Uau, você gosta mesmo de falar sobre a sua banda, porém esse campo só aceita até 2200 caracteres.'),
+
+    // Validando o campo Site
+    check('site').trim()
+        .isLength({ max: 100 }).withMessage('Tem certeza que esse é o seu site? Este campo só aceita até 100 caracteres.'),
+    
+    // Validando o campo Canal
+    check('canal').trim()
+        .isLength({ max: 100 }).withMessage('Tem certeza que esse é o seu canal? Este campo só aceita até 100 caracteres.')
+],
+BandaMiddleware.error, 
+perfilEditarBandaController.change);
 
 module.exports = router;
