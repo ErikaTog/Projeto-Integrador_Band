ea5d58aa3bcb40bc536e4435e7125c6fa1235ba7
andressa fazam <andressafazam@gmail.com>
2020-05-30 00:34:46 -0300

    Ajustes - item cidade do banco de dados

--- band_express/middlewares/PerfilEditarEstab.js
+++ band_express/middlewares/PerfilEditarEstab.js
@@ -12,7 +12,7 @@ const perfilEditarEstab = {
                 nome: req.session.usuario.nome 
             },
             raw: true,
-            attributes: ['nome', 'avatar', 'wallpaper', 'cidade.nome', 'cidade.estado.uf'],
+            attributes: ['nome', 'avatar', 'wallpaper', 'cidade.cidade', 'cidade.estado.uf'],
             include: [{
                 model: Cidade,
                 as: 'cidade',
