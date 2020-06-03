53c8bd0cb7fae86d070529945231b4047c1090e2
ErikaTog <erika.togniolo@gmail.com>
2020-06-03 16:50:57 -0300

    incluindo middlwarea para áudio

---
+++ band_express/middlewares/multerAudio.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const multer = require('multer');
+
+
+const multerAudio = {
+    
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, './public/img/audio')
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${file.fieldname}${req.session.usuario.id_usuario}-${Date.now()}${path.extname(file.originalname)}`)
+        }
+    }),
+
+    limits: {
+        fileSize: 4 * 1024 * 1024 * 1024,
+        files: 1
+    },
+
+    fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+            'audio/mp3',
+            'audio/AAC',
+            'audio/WMA',
+            'audio/WAVE',
+            'audio/AIFF',
+            'audio/Ogg',   
+        ]
+        if (allowedMimes.includes(file.mimetype)) {
+            cb(null, true);
+        }else{
+            cb(null, false);
+            console.log("Erro de arquivo não suportado");
+        }
+    }
+
+}
+
+module.exports = multerAudio;
