5b90b96d90f74372e674c4a6249ae53f6236f83a
ErikaTog <erika.togniolo@gmail.com>
2020-06-08 00:08:26 -0300

    incluindo método showUser

--- band_express/controllers/PerfilBandaController.js
+++ band_express/controllers/PerfilBandaController.js
@@ -4,6 +4,144 @@ const {Usuario, Banda, BandaIntegrantes, Cidade, Estado, Minha_rede, Audio, Vide
 
 
 const perfilBandaController = {
+
+    showUser: async (req, res) => {
+
+        try {  
+
+            //Pegando o id_usuario
+            let id_usuario = req.session.usuario.id_usuario
+            console.log('Id_usuario: ' + id_usuario)
+
+            // Selecionando os dados da banda na tabela banda
+            const dadosBanda = await Banda.findOne({
+                raw: true,
+                attributes: ['id_banda', 'genero', 'sobre', 'site', 'canal', 'id_usuario'],
+                where: {
+                    id_usuario
+                }
+            })
+            console.log(dadosBanda)
+
+            // Selecionando demais dados da banda nas tabelas usuario, cidade e estado
+            const dadosUsuarioBanda = await Usuario.findOne({
+                raw: true,
+                attributes: ['nome', 'email', 'avatar', 'wallpaper', 'cidade.cidade', 'cidade.estado.uf'],
+                include: [{
+                    attributes: [],
+                    model: Cidade,
+                    as: 'cidade',
+                    include: [{
+                        attributes: [],
+                        model: Estado,
+                        as: 'estado'
+                    }]
+                }],
+                where: {
+                    id_usuario
+                }
+            })
+            console.log(dadosUsuarioBanda)
+
+            
+            // Selecionando o nome, função e avatar dos integrantes
+            const integrantes = await BandaIntegrantes.findAll({
+                raw: true,
+                attributes: ['integrantesUsuario.nome', 'funcao', 'integrantesUsuario.avatar'],
+                include: [{
+                    attributes: [],
+                    model: Usuario,
+                    as: 'integrantesUsuario'
+                }],
+                where: {
+                    id_banda: dadosBanda.id_banda
+                }
+            })
+            console.log(integrantes)
+
+            
+            //Verificando a quantidade de usuários que a banda está seguindo
+            let seguindo = await Minha_rede.count({
+                where: {
+                    id_usuario
+                }
+            });
+            console.log('Seguindo: '+ seguindo)
+
+
+
+            //Verificando a quantidade de seguidores da banda
+            let seguidores = await Minha_rede.count({
+                where: {
+                    id_usuario_seguido: id_usuario
+                }
+            });
+            console.log('Seguidores: '+ seguidores)
+
+            //Pegando os vídeos postados
+            const videos = await Video.findAll({
+                raw: true,
+                attributes: ['tipo', 'titulo', 'caminho'],
+                where: {
+                    id_usuario
+                }
+            })
+            console.log(videos)
+            
+            //Pegando os áudios postados
+            const audios = await Audio.findAll({
+                raw: true,
+                attributes: ['tipo', 'titulo', 'caminho'],
+                where: {
+                    id_usuario
+                }
+            })
+            console.log(audios) 
+
+            return res.render('perfil-banda', {
+                title: 'Perfil',
+                usuario: req.session.usuario,
+                dadosBanda,
+                dadosUsuarioBanda,
+                integrantes,
+                seguindo,
+                seguidores,
+                videos,
+                audios,
+                errors: req.flash('errorValidator'),
+            });
+
+        } catch (error) {
+            return res.redirect('/home');
+        }    
+    },
+
+    changePassword: async (req, res) => {
+        let { senhaNova } = req.body;
+        
+        senhaNova = bcrypt.hashSync(senhaNova, 10);
+
+        const senhaBD = await Usuario.findOne({ 
+            where: { 
+                id_usuario: req.session.usuario.id_usuario 
+            }
+        });
+
+        senhaBD.senha = senhaNova;
+
+        await senhaBD.save({ fields: ['senha'] });
+
+        const dadosBanda = await Banda.findOne({ 
+            raw: true,
+            attributes: ['id_banda'],
+            where: { 
+                id_usuario: req.session.usuario.id_usuario 
+            } 
+        });
+
+        res.redirect(`/perfil/banda`);
+    },
+
     show: async (req, res) => {
 
         try {    
@@ -68,7 +206,7 @@ const perfilBandaController = {
             //Verificando a quantidade de seguidores da banda
             let seguidores = await Minha_rede.count({
                 where: {
-                    'id_usuario_seguido': dadosBanda.id_usuario
+                    id_usuario_seguido: dadosBanda.id_usuario
                 }
             });
             // console.log('Seguidores: '+ seguidores)
@@ -112,31 +250,6 @@ const perfilBandaController = {
         } catch (error) {
             return res.redirect('/home');
         }
-    },
-    changePassword: async (req, res) => {
-        let { senhaNova } = req.body;
-        
-        senhaNova = bcrypt.hashSync(senhaNova, 10);
-
-        const senhaBD = await Usuario.findOne({ 
-            where: { 
-                id_usuario: req.session.usuario.id_usuario 
-            }
-        });
-
-        senhaBD.senha = senhaNova;
-
-        await senhaBD.save({ fields: ['senha'] });
-
-        const dadosBanda = await Banda.findOne({ 
-            raw: true,
-            attributes: ['id_banda'],
-            where: { 
-                id_usuario: req.session.usuario.id_usuario 
-            } 
-        });
-
-        res.redirect(`/perfil/banda/${dadosBanda.id_banda}`);
     }
 }
 
