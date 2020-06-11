ea8fa6d4f27e7a94948f0a949ce785c93d9e9f24
andressa fazam <andressafazam@gmail.com>
2020-06-11 15:57:33 -0300

    Feito os metodos view e excluirUsuario

--- band_express/controllers/RemoverController.js
+++ band_express/controllers/RemoverController.js
@@ -1,9 +1,27 @@
+const { Usuario } = require('../models'); 
+
 const removerController = {
     view: async (req, res) => {
 
-        return res.render('remover', { title: 'Gerenciar', usuario: req.session.usuario });
+        return res.render('remover', { 
+            title: 'Gerenciar', 
+            usuario: req.session.usuario,
+            errors: req.flash('errorValidator')
+        });
+    },
+
+    excluirUsuario: async (req, res) => {
+
+        // let { motivo, senhaExcluir } = req.body;
+        const dadosUsuario = await Usuario.destroy({
+            where: {
+                id_usuario:  req.session.usuario.id_usuario
+            }
+        });
         
-    }
+        return res.redirect('/');
+        
+    },
 } 
 
 module.exports = removerController;
