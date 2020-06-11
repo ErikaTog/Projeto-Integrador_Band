7eea5cd9953e3970242922cacc0852e12eacb5d4
Mitsu325 <patricia.mashiba@gmail.com>
2020-06-10 21:07:38 -0300

    Busca dos novos usuários

--- band_express/controllers/HomeController.js
+++ band_express/controllers/HomeController.js
@@ -1,4 +1,6 @@
 const { Usuario, Minha_rede } = require('../models');
+const { Op } = require('sequelize');
+const moment = require('moment');
 
 const homeController = {
     view: async (req, res) => {
@@ -15,15 +17,27 @@ const homeController = {
         const seguindo = await Minha_rede.count({ where: { id_usuario_seguido: req.session.usuario.id_usuario } });
 
         // Novos usuários >> nome, avatar, link
+        const novosUsuarios = await Usuario.findAll({
+            where: {
+                data_cadastro: {
+                    [Op.gte]: moment().subtract(7, 'days').toDate()
+                }
+            },
+            raw: true,
+            attributes: ['nome', 'avatar', 'link_perfil', 'data_cadastro'],
+            limit: 10,
+            order: [['data_cadastro', 'DESC']]
+        })
 
-        // Post ordenados pelos mais recentes
+        // Post ordenados pelos mais recentes 
 
         return res.render('feed', { 
             title: 'Band+', 
             usuario: req.session.usuario,
             link: link.link_perfil,
             seguidores,
-            seguindo
+            seguindo,
+            novosUsuarios
         });
     }
 } 
