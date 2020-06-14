04f3ad0c9700fe2af4f353c4ac53a58265a09edf
Mitsu325 <patricia.mashiba@gmail.com>
2020-06-13 21:04:05 -0300

    Alterado o modo de salvar no BD

--- band_express/controllers/CadastroMusicoController.js
+++ band_express/controllers/CadastroMusicoController.js
@@ -17,13 +17,13 @@ const cadastroMuicoController = {
         // Buscar todos os instrumentos
         const instrumentos = await Instrumento.findAll({ 
             raw: true,
-            attributes: ['instrumento'] 
+            order: [['instrumento', 'ASC']]
         });
 
         // Buscar todas as habilidades
         const tecnicos = await Tecnico.findAll({ 
             raw: true,
-            attributes: ['habilidade_tecnica'] 
+            order: [['habilidade_tecnica', 'ASC']]
         });
 
         return res.render('form-musico', { 
@@ -35,7 +35,7 @@ const cadastroMuicoController = {
     },
     saveMusician: async (req, res) => {
 
-        let { nome, senha, email, sexo, sobre, estado, cidade, site, canal, canto, toco, tecnico, instrumento, habilidadeTecnica } = req.body;
+        let { nome, senha, email, sexo, sobre, estado: id_estado, cidade: id_cidade, site, canal, canto, toco, tecnico, instrumento: id_instrumento, habilidadeTecnica: id_tecnico } = req.body;
 
         nome = nome.trim();
         senha = senha.trim();
@@ -45,34 +45,14 @@ const cadastroMuicoController = {
         site = site ? site.trim(): '';
         canal = canal ? canal.trim(): '';
 
-        // Buscando o id_cidade e id_estado na tabela cidade
-        const findIdCidade = await Cidade.findAll({
-            //Inner join
-            include: [{
-                // com a tabela Estado
-                model: Estado, 
-                // utilizando a chave estabelecida na associação do model Cidade com Estado, cujo alias é 'estado'
-                as: 'estado',
-                // com um filtro adicional do estado que o usuário digitou
-                where: {
-                    uf: estado
-                }
-                }],
-                // e um último filtro da cidade que o usuário digitou
-            where: { cidade },
-        });
-        
-        const idCidade = findIdCidade[0].dataValues.id_cidade;
-        const idEstado = findIdCidade[0].dataValues.id_estado;
-
         // Inserindo informação na tabela usuario   
         const dadosUsuario = await Usuario.create({
             nome,
             email,
             senha,
             data_cadastro: new Date(),
-            id_cidade: idCidade,
-            id_estado: idEstado,
+            id_cidade,
+            id_estado,
             link_perfil: nome,
             id_tipos_perfil: 1
         })
@@ -94,37 +74,19 @@ const cadastroMuicoController = {
         await dadosUsuario.save({ fields: ['link_perfil'] });
 
         if (toco) {
-            // Buscando o id_instrumento
-            const findIdInstrumento = await Instrumento.findOne({
-                where: {
-                    instrumento
-                }
-            });
-    
-            const idInstrumento = findIdInstrumento.dataValues.id_instrumento;
-            
-            // Inserindo id_instrumento nas tabelas intermediárias
-            const dadosToco = await MusicoInstrumentos.create({
+            // Inserindo id_instrumento na tabela intermediária
+            await MusicoInstrumentos.create({
                 id_musico: dadosMusico.id_musico,
-                id_instrumento:  idInstrumento
+                id_instrumento
             });
         }
 
         if (tecnico) {
-            // Buscando o id_tecnico
-            const findIdTecnico = await Tecnico.findOne({
-                where: {
-                    habilidade_tecnica: habilidadeTecnica
-                }
-            });
-    
-            const idTecnico = findIdTecnico.dataValues.id_tecnico;
-            
-            // Inserindo id_tecnico nas tabelas intermediárias
-            const dadosTecnicos = await MusicoTecnicos.create({
+            // Inserindo id_tecnico na tabela intermediária
+            await MusicoTecnicos.create({
                 id_musico: dadosMusico.id_musico,
-                id_tecnico:  idTecnico
-                });
+                id_tecnico
+            });
         }
 
         // Setar session do usuario
