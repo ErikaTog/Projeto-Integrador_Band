c455980959a38fa44f900d3002044da1af2b769c
Mitsu325 <patricia.mashiba@gmail.com>
2020-06-19 21:05:15 -0300

    Removido código comentado

--- band_express/public/javascripts/btnSeguirPerfil.js
+++ band_express/public/javascripts/btnSeguirPerfil.js
@@ -8,39 +8,12 @@ const elementoButtonNaoSegue = () => {
     buttonSeguir.value = 0;
     buttonSeguir.classList.remove("btn-seguindo");
 
-    // grupoSeguir.innerHTML = '';
-
-    // let button = document.createElement('button');
-    // button.type = 'button';
-    // button.id = 'btnSeguir';
-    // button.className = 'btn-config';
-    // button.value = 0;
-
-    //     let iconeFone = document.createElement('i');
-    //     iconeFone.className = 'fas fa-headphones-alt';
-    //     button.appendChild(iconeFone);
-
-    // grupoSeguir.appendChild(button);
 }
 
 // Criando o button seguir
 const elementoButtonSegue = () => {
     buttonSeguir.value = 1;
     buttonSeguir.classList.add("btn-seguindo");
-
-    // grupoSeguir.innerHTML = '';
-    
-    // let button = document.createElement('button');
-    // button.type = 'button';
-    // button.id = 'btnSeguir';
-    // button.className = 'btn-config btn-seguindo';
-    // button.value = 1;
-
-    //     let iconeFone = document.createElement('i');
-    //     iconeFone.className = 'fas fa-headphones-alt';
-    //     button.appendChild(iconeFone);
-
-    // grupoSeguir.appendChild(button);
 }
 
 // BD >> deixar de seguir
